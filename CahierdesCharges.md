d341800b9519715175f58d74228654d87ff75fae
Minirius <marius.nerantzakis@gmail.com>
2023-12-04 15:17:56 +0100

    commit

---
+++ CahierdesCharges.md
@@ -0,0 +1,13 @@
+# Cahier des Charges
+
+Voici le cahier dse charges de l'application.
+
+## Entrée - Sortie 
+
+Le programme principal attend en entrée un fichier audio au format .wav (a voir pour integrer tout les fichier audios.)
+
+Ensuite, après traitement, le programme sort un fichier midi avec les notes et les rythmes de piano de la musique en mp3
+
+## Fonctionnement
+
+Le programme va donc analyser les fréquence de la musique et les convertir en donnée midi pour ainsi pouvoir utiliser les notes pour de la productyion musicale ou d'autres utlitées
--- app/main.py
+++ app/main.py
@@ -5,4 +5,19 @@
 #
 #   This is the Main app for the Music AI project
 #########################################################################################
-import numpy
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as pl
+
+rate, data = wavfile.read('Married-Life-piano.wav')
+
+temp_data = data
+data = data[:,0]
+#data = np.expand_dims(data, np.full(temp_data.shape, np.mean(data[196000: 200000])))
+print(data.shape, data.size, data.size)
+#for i in range(1.81e5, 160010):
+#    print(data[i])
+
+pl.plot(data[196000: 200000])
+pl.axis
+pl.show()
