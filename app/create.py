6e5690ad8525a923f01c1119cd0830254b8011d0
Minirius <marius.nerantzakis@gmail.com>
2023-12-07 15:55:03 +0100

    changement

--- app/create.py
+++ app/create.py
@@ -1,13 +1,13 @@
 import math
 import wave
 import struct
+import random
 
 if __name__ == '__main__':
     # http://stackoverflow.com/questions/3637350/how-to-write-stereo-wav-files-in-python
     # http://www.sonicspot.com/guide/wavefiles.html
-    freq = 440.0
-    data_size = 2000
-    fname = "test2.wav"
+    data_size = 10000
+    fname = "test3.wav"
     frate = 11025.0
     amp = 64000.0
     nchannels = 1
@@ -16,19 +16,19 @@ if __name__ == '__main__':
     nframes = data_size
     comptype = "NONE"
     compname = "not compressed"
-    data = [math.sin(2 * math.pi * freq * (x / frate))
-            for x in range(data_size)]
+    notes_freq = [random.randint(0, 1000) for i in range(5)]
+    print(notes_freq)
+    data = []
+    i = 0
+    for x in range(data_size):
+        freq = notes_freq[i]
+        data.append(math.sin(2 * math.pi * freq * (x / frate)))
+        if(x%2000 == 0 and x!=0):
+            i+=1
+    
     wav_file = wave.open(fname, 'w')
     wav_file.setparams(
         (nchannels, sampwidth, framerate, nframes, comptype, compname))
-    for v in data:
-        wav_file.writeframes(struct.pack('h', int(v * amp / 2)))
-    data = [math.sin(2 * math.pi * 554.37 * (x / frate))
-            for x in range(data_size)]
-    for v in data:
-        wav_file.writeframes(struct.pack('h', int(v * amp / 2)))
-    data = [math.sin(2 * math.pi * 659.255 * (x / frate))
-            for x in range(data_size)]
     for v in data:
         wav_file.writeframes(struct.pack('h', int(v * amp / 2)))
     wav_file.close()
--- app/main.py
+++ app/main.py
@@ -31,16 +31,14 @@ def get_freq(bit, chunk, offset, data, rate):
     if(idx <= len(freqs)):
         freq = freqs[idx]
         freq_in_hertz = abs(freq * rate)
-        return idx, freq, freq_in_hertz, rate
-    else:
-        return idx, 0, 0, rate
+        return freq_in_hertz
+
 
 def detection(show):
     rate, data = wavfile.read('flute.wav')
 
 
     #data = np.expand_dims(data, np.full(temp_data.shape, np.mean(data[196000: 200000])))
-    print(data.shape, data.size)
     # 15ms chunk includes delimiting 5ms 1600hz tone
     duration = 0.015
     # calculate the length of our chunk in the np.array using sample rate
@@ -50,14 +48,37 @@ def detection(show):
     # number of bits in the audio data to decode
     bits = int(len(data) / chunk)
 
-    decoded_freqs = [get_freq(bit, chunk, offset, data, rate) for bit in range(bits)]
+    decoded_freqs = []
+    for bit in range(bits):
+        freqq = get_freq(bit, chunk, offset, data, rate)
+        if freqq is not None :decoded_freqs.append(freqq)
+        else: decoded_freqs.append(decoded_freqs[-1])
+    mean_freq = []
+
+    joined_array = np.zeros(shape=(2, data.size))
+
+    precision = 60
+    for i in range(precision, bits, precision):
+        for j in range(precision):
+            mean_freq.append(np.mean(decoded_freqs[(i-precision) : i]))
 
     '''for bit in range(bits):
         print(get_freq(bit, chunk, offset, data, rate))
         time.sleep(0.05)'''
-    print(decoded_freqs)
+    #print(decoded_freqs)
 
-    if show: showGraphic(decoded_freqs);showGraphic(data)
+    if show:
+        pl.figure(0)
+        pl.plot(decoded_freqs)
+        pl.figure(1)
+        pl.plot(mean_freq)
+        pl.figure(2)
+        pl.plot(data)
+
+
+        pl.show()
+
+    print(data.size, len(decoded_freqs), data.shape)
     return data
 
 if __name__ == "__main__":
